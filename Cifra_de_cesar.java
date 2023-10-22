eebdb3acaf5fca2265ff92feea0af34ce271dcbc
renatata_a_rata <129771759+Renatata-H@users.noreply.github.com>
2023-10-21 21:25:23 -0300

    Update Cifra_de_cesar.java

--- Cifra_de_cesar.java
+++ Cifra_de_cesar.java
@@ -63,6 +63,74 @@ public class Cifra_de_cesar {
         return -1;
     }
 
+    public static String 
+    criptografa(int chave, String texto, int tamanho_texto, String[] alfabeto, String[] ALFABETO) {
+
+        int i = 0, letra, posição_alfabeto;
+        String novo_texto = "";
+
+        while (i < tamanho_texto) {
+            letra = texto.charAt(i);
+
+            if (letra >= 65 && letra <= 90) {
+                // Letra maiúscula;
+                posição_alfabeto = busca_binaria(ALFABETO, ""+texto.charAt(i));
+                posição_alfabeto = (posição_alfabeto + chave)%26;
+                if (posição_alfabeto < 0) {
+                   posição_alfabeto = posição_alfabeto + 26;
+                }
+                novo_texto = novo_texto + ALFABETO[posição_alfabeto];
+
+            } else if (letra >= 97 && letra <= 122) {
+                // Letra minúscula;
+                posição_alfabeto = busca_binaria(alfabeto, ""+texto.charAt(i));
+                posição_alfabeto = (posição_alfabeto + chave)%26;
+                if (posição_alfabeto < 0) {
+                    posição_alfabeto = posição_alfabeto + 26;
+                }
+                novo_texto = novo_texto + alfabeto[posição_alfabeto];
+            } else {
+                novo_texto = novo_texto + texto.charAt(i);
+            }
+            i = i + 1;
+        }
+        return novo_texto;
+    } 
+
+    public static String 
+    descriptografa(int chave, String texto, int tamanho_texto, String[] alfabeto, String[] ALFABETO) {
+
+        int i = 0, letra, posição_alfabeto;
+        String novo_texto = "";
+
+        while (i < tamanho_texto) {
+            letra = texto.charAt(i);
+
+            if (letra >= 65 && letra <= 90) {
+                // Letra maiúscula;
+                posição_alfabeto = busca_binaria(ALFABETO, ""+texto.charAt(i));
+                posição_alfabeto = (posição_alfabeto-chave)%26;
+                if (posição_alfabeto < 0) {
+                   posição_alfabeto = posição_alfabeto + 26;
+                }
+                novo_texto = novo_texto + ALFABETO[posição_alfabeto];
+
+            } else if (letra >= 97 && letra <= 122) {
+                // Letra minúscula;
+                posição_alfabeto = busca_binaria(alfabeto, ""+texto.charAt(i));
+                posição_alfabeto = (posição_alfabeto-chave)%26;
+                if (posição_alfabeto < 0) {
+                   posição_alfabeto = posição_alfabeto + 26;
+                }
+                novo_texto = novo_texto + alfabeto[posição_alfabeto];
+            } else {
+                novo_texto = novo_texto + texto.charAt(i);
+            }
+            i = i + 1;
+        }
+        return novo_texto;
+    } 
+
     public static void 
     main(String args[]) {
         /* Dado como argumento a chave, um modo de execução e um texto
@@ -71,77 +139,30 @@ public class Cifra_de_cesar {
          */
 
         int chave = Integer.parseInt(args[0]);
-        int i = 0, letra, posição_alfabeto;
-
         String texto = args[2];
         int tamanho_texto = texto.length();
         String novo_texto = "";
-
+        
         String[] alfabeto = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
         String[] ALFABETO = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
 
         String modo = args[1];
+        System.out.println(modo);
 
         // Criptografa
         if (ordem_lexicografica(args[1],"criptografa") == 0) {
-
-            while (i < tamanho_texto) {
-                letra = texto.charAt(i);
-
-                if (letra >= 65 && letra <= 90) {
-                    // Letra maiúscula;
-                    posição_alfabeto = busca_binaria(ALFABETO, ""+texto.charAt(i));
-                    posição_alfabeto = (posição_alfabeto + chave)%26;
-                    novo_texto = novo_texto + ALFABETO[posição_alfabeto];
-
-                } else if (letra >= 97 && letra <= 122) {
-                    // Letra minúscula;
-                    posição_alfabeto = busca_binaria(alfabeto, ""+texto.charAt(i));
-                    posição_alfabeto = (posição_alfabeto + chave)%26;
-                    if (posição_alfabeto < 0) {
-                        posição_alfabeto = posição_alfabeto + 26;
-                    }
-                    novo_texto = novo_texto + alfabeto[posição_alfabeto];
-                } else {
-                    novo_texto = novo_texto + texto.charAt(i);
-                }
-
-                i = i + 1;
-            }
+            novo_texto = criptografa(chave, texto, tamanho_texto, alfabeto, ALFABETO);
             System.out.println(novo_texto);
-        } 
+        
+        }
         // Descriptografa
         else if (ordem_lexicografica(args[1],"descriptografa") == 0) {
-
-            while (i < tamanho_texto) {
-                letra = texto.charAt(i);
-
-                if (letra >= 65 && letra <= 90) {
-                    // Letra maiúscula;
-                    posição_alfabeto = busca_binaria(ALFABETO, ""+texto.charAt(i));
-                    posição_alfabeto = (posição_alfabeto-chave)%26;
-                    novo_texto = novo_texto + ALFABETO[posição_alfabeto];
-
-                } else if (letra >= 97 && letra <= 122) {
-                    // Letra minúscula;
-                    posição_alfabeto = busca_binaria(alfabeto, ""+texto.charAt(i));
-                    posição_alfabeto = (posição_alfabeto-chave)%26;
-                    if (posição_alfabeto < 0) {
-                        posição_alfabeto = posição_alfabeto + 26;
-                    }
-                    novo_texto = novo_texto + alfabeto[posição_alfabeto];
-                } else {
-                    novo_texto = novo_texto + texto.charAt(i);
-                }
-                i = i + 1;
-            }
+            novo_texto = descriptografa(chave, texto, tamanho_texto, alfabeto, ALFABETO);
             System.out.println(novo_texto);
-            
-        
         } 
         // Inválido 
-        //else {
-        //    System.out.println("Modo de execução inválido!");
-        //}
+        else {
+            System.out.println("Modo de execução inválido!");
+        }
     }
 }
